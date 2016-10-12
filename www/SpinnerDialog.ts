e368961e1a23d2ceba17c8798c7825c046d1764c
Yonggang Luo <luoyonggang@gmail.com>
2016-10-13 00:44:19 +0800

    Installing the plugin properly.

---
+++ www/SpinnerDialog.ts
@@ -0,0 +1,19 @@
+import { Cordova, Plugin } from 'ionic-native';
+
+@Plugin({
+  plugin: 'com.filfatstudios.spinnerdialog',
+  pluginRef: 'com.filfatstudios.spinnerdialog',
+  repo: 'https://github.com/acrop/cordova-plugin-spinnerdialog',
+  platforms: ['Android', 'iOS']
+})
+export class SpinnerDialog {
+  @Cordova({
+    sync: true
+  })
+  static show(title: string, message: string, cancelCallback: boolean | ((data: any) => any)): void {};
+
+  @Cordova({
+    sync: true
+  })
+  static hide(success: () => void, fail: () => void): void {};
+}
